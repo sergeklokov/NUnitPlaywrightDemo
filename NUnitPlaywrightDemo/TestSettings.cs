2c43fa5c2f9ebea42c054351910551c7adfec33c
sam-klok <sklok11@gmail.com>
2025-04-12 17:41:04 -0700

    + reading settings from appsettings.json

---
+++ NUnitPlaywrightDemo/TestSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitPlaywrightDemo
+{
+    public class TestSettings
+    {
+        public string BaseUrl { get; set; }
+    }
+}
--- NUnitPlaywrightDemo/UnitTest1.cs
+++ NUnitPlaywrightDemo/UnitTest1.cs
@@ -1,13 +1,37 @@
+using Microsoft.Extensions.Configuration;
+
 namespace NUnitPlaywrightDemo
 {
     [Parallelizable(ParallelScope.Self)]
     [TestFixture]
     public class Tests : PageTest
     {
+        private TestSettings _settings;
+
+        [OneTimeSetUp]
+        public async Task OneTimeSetUp()
+        {
+            // Load configuration
+            IConfiguration config = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            _settings = config.GetSection("TestSettings").Get<TestSettings>();
+
+            // Initialize Playwright
+            //var playwright = await Playwright.CreateAsync();
+            //_apiContext = await playwright.APIRequest.NewContextAsync(new()
+            //{
+            //    IgnoreHTTPSErrors = true // For self-signed certificate
+            //});
+        }
+
         [Test]
         public async Task HomepageHasPlaywrightInTitleAndGetStartedLinkLinkingtoTheIntroPage()
         {
-            await Page.GotoAsync("https://playwright.dev");
+            //await Page.GotoAsync("https://playwright.dev");
+            await Page.GotoAsync(_settings.BaseUrl);
 
             // Expect a title "to contain" a substring.
             await Expect(Page).ToHaveTitleAsync(new Regex("Playwright"));
